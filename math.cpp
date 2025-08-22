d5ceed6f8aca14dfe55dc16b03e4ccbda98f11a2
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-23 00:17:24 +0600

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    // cout << min(5,20);
+    // cout << max(8,56);
+
+    // cout << sqrt(9);
+    //cout <<pow(2,2);
+    //cout << round(3.45);
+    //cout << log(6);
+    return 0;
+}
