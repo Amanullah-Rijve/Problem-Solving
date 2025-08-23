426d340edc82120d800e0d531803940105f5938a
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-24 00:47:23 +0600

    Add files via upload

---
+++ userInput.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int a;
+    // cout << " Enter A integer: ";
+    // cin >> a;
+    // cout << " you have entered " << a;
+
+    int sum,a,b;
+    cout << " Enter first integer: ";
+    cin >> a;
+
+    cout << " Enter Second integer:  ";
+    cin >> b;
+
+    sum= a+b;
+    cout << "The Sum is: " << sum;
+
+    return 0;
+
+}
