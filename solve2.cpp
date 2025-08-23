941560bc2318b4d32f4839e274a6e599080e9482
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-24 00:48:09 +0600

    Add files via upload

---
+++ solve2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cout << "Enter first int: ";
+    cin >> a;
+    cout << "Enter second int: ";
+    cin >> b;
+    cout << "Enter third int: ";
+    cin >> c;
+
+    int sum = a+b+c;
+    cout << sum;
+
+    return 0;
+}
