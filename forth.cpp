b6d8c832e99bde7e7ca0c671309be347ab27f4a2
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-25 00:30:29 +0600

    Add files via upload

---
+++ forth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int a = 6,  b = 7, c = 20;
+    // cout << a + b + c;
+
+    // int x, y, z;
+    // x=y=z=79;
+    // cout << x+y+z;
+
+    const int min = 60;
+    min = 70; // this line will show error
+    cout << min;
+
+    return 0;
+}
