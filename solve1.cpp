091ab82c527be12e0f686df2b153644e03c3e83a
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-26 01:05:28 +0600

    Add files via upload

--- solve1.cpp
+++ solve1.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 
-    cout << "Hello, World!";
+    int n;
+    cin >> n;
+
+    string word[]= {"one","two","three","four","five","six","seven","eight","nine"};
+
+    if(n>=1 && n<=9){
+        cout << word[n-1];
+    }else if(n==0){
+        cout << "Its a 0";
+    }
+    else{
+        cout << "grater than 9";
+    }
+
 
     return 0;
 }
