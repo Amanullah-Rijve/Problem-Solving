dc30e6d197bfb8b0a2c7078468f6861cfcf277ba
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-03 18:58:37 +0600

    Add files via upload

--- solve1.cpp
+++ solve1.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
 
-    int n;
-    cin >> n;
+    int a,b;
+    cin >> a ;
+    cin >> b;
 
-    string word[]= {"one","two","three","four","five","six","seven","eight","nine"};
+    string words[]={"one","two","three","four","five","six","seven","eight","nine"};
 
-    if(n>=1 && n<=9){
-        cout << word[n-1];
-    }else if(n==0){
-        cout << "Its a 0";
+    for(int i=a;i<=b;i++){
+        if(i>=1 && i<=9){
+            cout << words[i-1] <<endl;
+        }else{
+            if(i%2==0){
+                cout <<"Even"<<endl;
+            }else{
+                cout << "odd" <<endl;
+            }
+        }
     }
-    else{
-        cout << "grater than 9";
-    }
-
 
     return 0;
 }
