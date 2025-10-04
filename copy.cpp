51f36c624339219043e7c87c9de9d3625e4c8a81
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-10-04 15:58:17 +0600

    Create copy.cpp

---
+++ copy.cpp
@@ -0,0 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day = 1;
+    switch(day){
+        case 1:
+        cout<<"Sunday";
+        break;
+        case 2:
+        cout<<"Monday";
+        break;
+        case 3:
+        cout<<"Thuesday";
+        break;
+        case 4:
+        cout<<"wednseday";
+         break;
+        case 5:
+        cout<<"thursday";
+         break;
+        case 6:
+        cout<<"Friday";
+         break;
+        default:
+        cout<<"Enter again";
+    }
+    
+    return 0;
+}
