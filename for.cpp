cd7197690016beaaa129939c0c08d6c795b93bd9
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-10-07 00:04:06 +0600

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+   int tusty = 1;
+   for(tusty ; tusty<=10000; tusty++){
+       cout<<tusty<<endl;
+   }
+
+    return 0;
+}
+
+// first statement
+
+// value = 0
+
+//  second statement
+//  condition 
+
+//  third statement 
+//  excutation time// value
+//  i = i+1 / i++
+
+//   increament
+// i =0
+// i = i+1 =0+1 =1
+// i = 1+1 = 2
+// i = 2+1=3
